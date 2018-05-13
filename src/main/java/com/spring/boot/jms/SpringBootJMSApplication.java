ffbec2d6dde47d5b73ba18802ece4f71e13dca6a
spandey280191 <35771851+spandey280191@users.noreply.github.com>
2018-05-13 22:44:02 +0200

    update

--- src/main/java/com/spring/boot/jms/SpringBootJMSApplication.java
+++ src/main/java/com/spring/boot/jms/SpringBootJMSApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HaqlxApplication {
+public class SpringBootJMSApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HaqlxApplication.class, args);
+		SpringApplication.run(SpringBootJMSApplication.class, args);
 	}
 }
